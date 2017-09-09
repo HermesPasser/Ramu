725393645f587ce20012f802f99a9a035e830e97
Hermes Passer <dsldouglas@hotmail.com>
2017-09-09 17:03:23 -0300

    1st commit.

---
+++ Ramu.js
@@ -0,0 +1,159 @@
+// --------------------------------- //
+// Ramu 0.1 - Hermes Passer in 08/09 //
+// --------------------------------- //
+
+var FRAME 	   = 1000/60;
+var DEBUG_MODE = true;
+
+var objsToDraw 	  = [];
+var objsToCollide = [];
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var lastKeyPressed;
+var inLoop = true;
+
+class GameObj{
+	constructor(){
+		this.tag = "";
+	}
+	
+	onStart() { } // Virtual
+	
+	onUpdate() { } // Virtual
+}
+
+class Drawble extends GameObj{
+	constructor(x, y, width, height){
+		super();
+		this.canDraw = true;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		
+		objsToDraw.push(this);
+	}
+	
+	drawInCanvas(){
+		if (this.canDraw) this.draw()
+	}
+	
+	draw(){ } // Virtual
+}
+
+// WORKING
+class Collisor extends Drawble{
+	constructor(x, y, width, height){
+		super(x,y, width, height);
+		this.isInCollision = false;
+		objsToCollide.push(this);
+	}
+	
+	onUpdate(){
+		this.canDraw = DEBUG_MODE;
+	}
+	
+	onCollision(){ } // Virtual
+
+	checkCollision(){
+		for (var i = 0; i < objsToCollide.length; i++){
+			if (objsToCollide[i] === this) // Para não testar consigo mesmo
+				continue;
+			
+			if (this.x < objsToCollide[i].x + objsToCollide[i].width &&
+				   this.x + this.width > objsToCollide[i].x &&
+				   this.y < objsToCollide[i].y + objsToCollide[i].height &&
+				   this.height + this.y > objsToCollide[i].y){
+			
+				this.isInCollision = true;
+				this.onCollision();
+			} else this.isInCollision = false;
+		}
+	}
+	
+}
+
+class RectCollisor extends Collisor{
+	constructor(x, y, width, height){
+		super(x,y, width, height);
+	}
+	
+	draw(){
+		if (!this.isInCollision){
+			ctx.strokeStyle = "blue";
+			ctx.strokeRect(this.x, this.y, this.width, this.height);
+		}
+		else{
+			ctx.strokeStyle = "red";
+			ctx.strokeRect(this.x, this.y, this.width, this.height);
+		}
+
+		ctx.strokeStyle = "#000000"; // reset default value
+	}
+}
+
+// END - WORKING 
+
+class Ramu{
+	 
+	static input(){
+		document.body.onkeypress = function(e) {
+			lastKeyPressed = (e.keyCode || e.which);
+		}
+	}
+	
+	static loop(){
+		
+		if (inLoop){
+			Ramu.onUpdate();
+			Ramu.checkCollision();
+			Ramu.draw();
+		}
+
+		window.requestAnimationFrame(Ramu.loop, FRAME);
+	}
+	
+	static stopLoop(){
+		inLoop = false;
+	}
+	
+	static resumeLoop(){
+		inLoop = true;
+	}
+	
+	static init(){
+		Ramu.input();
+		Ramu.onStart();
+		Ramu.loop();
+	}
+	
+	static onStart(){
+		for (var i = 0; i < objsToDraw.length; i++)
+			objsToDraw[i].onStart();	
+	}
+	
+	static onUpdate(){
+		for (var i = 0; i < objsToDraw.length; i++)
+			objsToDraw[i].onUpdate();	
+		lastKeyPressed = null;
+	}
+	
+	static checkCollision(){
+		for (var i = 0; i < objsToCollide.length; i++)
+			objsToCollide[i].checkCollision();
+	}
+	
+	static draw(){	
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		
+		for (var i = 0; i < objsToDraw.length; i++){
+			var positionWidth = objsToDraw[i].x + objsToDraw[i].width;		
+			var positionHeigh = objsToDraw[i].y + objsToDraw[i].height;
+			
+			if (positionWidth >= 0 && objsToDraw[i].x <= canvas.width && // Renderiza somente o que esta no canvas
+					positionHeigh>= 0 && objsToDraw[i].y <= canvas.height)
+				objsToDraw[i].drawInCanvas();
+		}
+	}
+}
---
+++ Samples/Examples.js
@@ -0,0 +1,66 @@
+class AnimatedRect extends RectCollisor{
+	constructor(x,y,width,height){
+		super(x,y, width, height);
+		this.forceX = 3;
+		this.forceY = 3;
+	}
+	
+	draw(){
+		ctx.strokeRect(this.x, this.y, this.width, this.height);
+	}
+	
+	onUpdate(){
+		if (this.x <= 0 || this.x >= canvas.width - this.width)
+			this.forceX = -(this.forceX);
+		
+		if (this.y <= 0 || this.y >= canvas.height - this.height)
+			this.forceY = -(this.forceY);
+		
+		this.x += this.forceX;
+		this.y += this.forceY;
+	}
+	
+	onCollision(){
+		this.forceX = -(this.forceX + (Math.random() * 3));
+		this.forceY = -(this.forceY + (Math.random() * 3));
+		
+	}
+}
+
+class MovingChar extends RectCollisor{
+	constructor(x,y,width,height){
+		super(x,y, width, height);
+		this.die = false;
+	}
+	
+	draw(){
+		super.draw();
+		if (this.die) ctx.fillText("*Died*",this.x, this.y);
+	}
+	
+	onUpdate(){
+		if (this.die) return;
+		super.onUpdate();
+		this.canDraw = true;
+		
+		if (lastKeyPressed == 97) // A
+			this.x -= 1;
+		else if (lastKeyPressed == 100) // D
+			this.x += 1;
+			
+		if (lastKeyPressed == 115) // S
+			this.y += 1;
+		else if (lastKeyPressed == 119) // W
+			this.y -= 1;
+	}
+	
+	onCollision(){
+		if (!this.die){
+			this.width--;
+			this.height--;
+		}	
+		
+		if (this.width == 0 && this.height == 0)
+			this.die = true;
+	}
+}
