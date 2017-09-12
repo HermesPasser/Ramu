2b07ea98d158dc4c644d7208804682d4095af93f
Hermes Passer <dsldouglas@hotmail.com>
2017-09-12 16:38:55 -0300

    GameSprite added.

--- Ramu.js
+++ Ramu.js
@@ -1,162 +1,62 @@
 // --------------------------------- //
-// Ramu 0.1 - Hermes Passer in 08/09 //
+// Ramu 0.2 - Hermes Passer in 08/09 //
 // --------------------------------- //
 
-var FRAME 	   = 1000/60;
-var DEBUG_MODE = false;
-
-var gameObjs	  = [];
-var objsToDraw 	  = [];
-var objsToCollide = [];
+var gameObjs	   = [];
+var objsToDraw 	   = [];
+var objsToCollide  = [];
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var lastKeyPressed;
-var inLoop = true;
 
-class GameObj{
-	constructor(){
-		this.tag = "";
-		gameObjs.push(this);
-	}
-	
-	onStart() { } // Virtual
-	
-	onUpdate() { } // Virtual
-}
-
-class Drawble extends GameObj{
-	constructor(x, y, width, height){
-		super();
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
-		this.canDraw = true;
-		
-		objsToDraw.push(this);
-		
-		Object.defineProperty(Drawble, 'drawInCanvas', {
-		  writeable: false,
-		});
-	}
-	
-	drawInCanvas(){
-		if (this.canDraw) this.draw()
-	}
-	
-	draw(){ } // Virtual
-}
-
-class Collisor extends Drawble{
-	constructor(x, y, width, height){
-		super(x,y, width, height);
-		this.canCollide = true;
-		this.collisor = null;
-		this.isInCollision = false;
-		objsToCollide.push(this);
-	}
-	
-	onUpdate(){
-		this.canDraw = DEBUG_MODE;
-	}
-	
-	onCollision(){ } // Virtual
-
-	checkCollision(){
-		if(!this.canCollide) return;
-		
-		for (var i = 0; i < objsToCollide.length; i++){
-			if (objsToCollide[i] === this || !objsToCollide[i].canCollide) // Para não testar consigo mesmo
-				continue;
-			
-			if (this.x < objsToCollide[i].x + objsToCollide[i].width &&
-				   this.x + this.width > objsToCollide[i].x &&
-				   this.y < objsToCollide[i].y + objsToCollide[i].height &&
-				   this.height + this.y > objsToCollide[i].y){
-			
-				this.isInCollision = true;
-				this.collisor = objsToCollide[i];
-				this.onCollision();
-			} else {
-				this.isInCollision = false;
-				this.collisor = null;
-			}
-		}
-	}
-}
-
-class SimpleRectCollisor extends Collisor{
-	constructor(x, y, width, height){
-		super(x, y, width, height);
-	}
-	
-	draw(){
-		if (!this.isInCollision)
-			ctx.strokeStyle = "blue";
-		else ctx.strokeStyle = "red";
-
-		ctx.strokeRect(this.x, this.y, this.width, this.height);
-		ctx.strokeStyle = "#000000"; // reset default value
-	}
-}
-
-class GameSprite extends Drawble{
-	constructor(x, y, url){ //  
-		super(x, y, 0, 0);
-		this.sprite = new Image();
-		this.sprite.src = url;
-		this.width = this.sprite.width;
-		this.height = this.sprite.height;
-	}
-	
-	draw(){
-		ctx.drawImage(this.sprite, 100, 100);
-	}
-}
-
-class Ramu{
-	 
-	static input(){
-		document.body.onkeypress = function(e) {
-			lastKeyPressed = (e.keyCode || e.which);
-		}
-	}
-	
-	static loop(){
-		
-		if (inLoop){
-			Ramu.onUpdate();
-			Ramu.checkCollision();
-			Ramu.draw();
-		} //else onStopLoop();
-		
-		window.requestAnimationFrame(Ramu.loop, FRAME);
-	}
-	
-	static stopLoop(){
-		inLoop = false;
-	}
-	
-	static resumeLoop(){
-		inLoop = true;
+class Ramu{	
+	constructor() {
+		throw new Error('This is a static class');
 	}
 	
 	static init(){
+		Ramu.debugMode = false;
+		Ramu.inLoop = true;
+		Ramu.delta = { last: 0, time: 0 };
+		Ramu.lastKeyPressed = null;
+		
 		Ramu.input();
-		Ramu.onStart();
-		Ramu.loop();
+		Ramu.start();
+		window.requestAnimationFrame(Ramu.loop);
 	}
 	
-	static onStart(){
-		for (var i = 0; i < gameObjs.length; i++)
-			gameObjs[i].onStart();	
+	static input(){
+		document.body.onkeypress = function(e) {
+			Ramu.lastKeyPressed = (e.keyCode || e.which);
+		}
+	}
+
+	static loop(){
+		// Calcule delta time
+		var t = Date.now();
+		Ramu.delta.time = t - Ramu.delta.last;
+		Ramu.delta.last = t;
+		
+		if (Ramu.inLoop){
+			Ramu.update();
+			Ramu.checkCollision();
+		}
+				
+		Ramu.draw();
+		
+		// To the input work even with update disbled
+		Ramu.lastKeyPressed = null;
+		window.requestAnimationFrame(Ramu.loop);
 	}
 	
-	static onUpdate(){
+	static start(){
 		for (var i = 0; i < gameObjs.length; i++)
-			gameObjs[i].onUpdate();	
-		lastKeyPressed = null;
+			gameObjs[i].start();	
+	}
+	
+	static update(){
+		for (var i = 0; i < gameObjs.length; i++)
+			gameObjs[i].update();	
 	}
 	
 	static checkCollision(){
@@ -175,5 +75,138 @@ class Ramu{
 					positionHeigh>= 0 && objsToDraw[i].y <= canvas.height)
 				objsToDraw[i].drawInCanvas();
 		}
+	}
+}
+
+class GameObj{
+	constructor(x = 0, y = 0){
+		this.x = x;
+		this.y = y;
+		this.tag =  this.tag || "none";
+		gameObjs.push(this);
+	}
+	
+	destroy(){
+		for (var i = 0; i < gameObjs.length; i++){
+			if (gameObjs[i] == this){
+				gameObjs.splice(i);
+				break;
+			}
+		}
+	} 
+	
+	start() { } // Virtual
+	
+	update() { } // Virtual
+	
+	// onKeypress() { } // Virtual
+}
+
+class Drawble extends GameObj{
+	constructor(x, y, width, height, canDraw = false){
+		super();
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.canDraw = true;
+		
+		objsToDraw.push(this);
+	}
+	
+	destroy(){
+		super.destroy();
+		for (var i = 0; i < objsToDraw.length; i++){
+			if (objsToDraw[i] == this){
+				objsToDraw.splice(i);
+				break;
+			}
+		}
+	}
+	
+	drawInCanvas(){
+		if (this.canDraw) this.draw();
+	}
+	
+	draw(){ } // Virtual
+}
+
+class Collisor extends Drawble{
+	constructor(x, y, width, height){
+		super(x,y, width, height);
+		this.canCollide = true;
+		this.collision = null;
+		this.isInCollision = false;
+		objsToCollide.push(this);
+	}
+	
+	destroy(){
+		super.destroy();
+		for (var i = 0; i < objsToCollide.length; i++){
+			if (objsToCollide[i] == this){
+				objsToCollide.splice(i);
+				break;
+			}
+		}
+	}
+	
+	update(){
+		this.canDraw = Ramu.debugMode;
+	}
+	
+	onCollision(){ } // Virtual
+
+	checkCollision(){
+		if(!this.canCollide) return;
+		
+		for (var i = 0; i < objsToCollide.length; i++){
+			if (objsToCollide[i] === this || !objsToCollide[i].canCollide) // Para não testar consigo mesmo
+				continue;
+			
+			if (this.x < objsToCollide[i].x + objsToCollide[i].width &&
+				   this.x + this.width > objsToCollide[i].x &&
+				   this.y < objsToCollide[i].y + objsToCollide[i].height &&
+				   this.height + this.y > objsToCollide[i].y){
+			
+				this.isInCollision = true;
+				this.collision = objsToCollide[i];
+				this.onCollision();
+			} else {
+				this.isInCollision = false;
+				this.collisor = null;
+			}
+		}
+	}
+}
+
+class SimpleRectCollisor extends Collisor{
+	constructor(x, y, width, height){
+		super(x, y, width, height);
+	}
+	
+	draw(){
+		if (Ramu.debugMode){
+			if (!this.isInCollision)
+				ctx.strokeStyle = "blue";
+			else 
+				ctx.strokeStyle = "red";
+
+			ctx.strokeRect(this.x, this.y, this.width, this.height);
+			ctx.strokeStyle = "#000000"; // reset default value
+		}
+	}
+}
+
+class GameSprite extends Drawble{
+	constructor(url, x, y, w, h, canDraw = true){
+		super(x, y, w, h);
+		this.img = new Image();
+		this.img.src = url;
+		this.canDraw = canDraw;
+	}
+	
+	draw(){
+		if (this.canDraw)
+			ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
 	}
 }
