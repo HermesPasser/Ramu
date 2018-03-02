06ad1b6fca0e94349aa448df195fdf023c461afe
Hermes Passer <dsldouglas@hotmail.com>
2018-03-02 16:48:36 -0300

    Add distance func in ramumath.

--- Ramu.js
+++ Ramu.js
@@ -54,6 +54,12 @@ class RamuMath{
 		throw new Error('This is a static class');
 	}
 	
+	static distance(gameObjectA, gameObjectB){
+		let x = Math.pow(gameObjectA.x - gameObjectB.x, 2),
+			y = Math.pow(gameObjectA.y - gameObjectB.y, 2);
+		return Math.sqrt(x + y, 2);
+	}
+	
 	static overlap(rect1, rect2) {
 		return(rect1.x < rect2.x + rect2.width &&
 			   rect1.x + rect1.width > rect2.x &&
