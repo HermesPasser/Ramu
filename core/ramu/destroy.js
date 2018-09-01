c41213a0a9b2b14b03a0ccae8f020a5e4f13a6e2
Hermes Passer <dsldouglas@hotmail.com>
2018-08-31 23:22:04 -0300

    removed some console.logs

--- core/ramu/destroy.js
+++ core/ramu/destroy.js
@@ -11,7 +11,6 @@ Ramu._destroyGameObj = function(){
 Ramu._destroyDrawable = function(){
 	for (let i = Ramu.objsToDraw.length - 1; i >= 0; --i){
 		if (Ramu.objsToDraw[i].canDestroy){
-			console.log(Ramu.objsToDraw);
 			Ramu.objsToDraw.splice(i, 1);
 		}
 	}
@@ -20,7 +19,6 @@ Ramu._destroyDrawable = function(){
 Ramu._destroyCollisor = function(){
 	for (let i = Ramu.objsToCollide.length - 1; i >= 0; --i){
 		if (Ramu.objsToCollide[i].canDestroy){
-			console.log(Ramu.objsToCollide);
 			Ramu.objsToCollide.splice(i, 1);
 		}
 	}
