e816019716cf2059046b9e06376a0a56925414b5
Hermes Passer <dsldouglas@hotmail.com>
2019-11-08 18:28:33 -0300

    add parent node for canvas argument

--- core/ramu/init.js
+++ core/ramu/init.js
@@ -13,15 +13,12 @@ Ramu.ctx = Ramu.canvas.getContext('2d');
 Ramu.time = {last: 0, delta: 1/60, frameTime: 0};
 
 /// Init the Ramu and the main loop.
-Ramu.init = function(width = 500, height = 500){
-	if (!document.body)
-		throw new Error('No body tag found.');
-	
-	// Ramu.canvas = document.createElement('canvas');
-	Ramu.canvas.width  = width
-	Ramu.canvas.height = height
-	// Ramu.ctx = Ramu.canvas.getContext('2d');
-	document.body.appendChild(Ramu.canvas);
+Ramu.init = function(width = 500, height = 500, parentNode = null){
+	Ramu.canvas.width  = width;
+	Ramu.canvas.height = height;
+
+	const exception = () => { throw new Error('No body tag found.'); };
+	(parentNode || document.body || exception()).appendChild(Ramu.canvas);
 	
 	Ramu.callSortUpdate    = false;
 	Ramu.callSortDraw 	   = false;
